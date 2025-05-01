9a80fe105d49ac8d805dce17b9b883ad6a00bd00
Peter Buckley <peterbuckley3636@gmail.com>
2025-05-01 02:43:33 -0600

    I2C Motor Working

--- Servo/main.c
+++ Servo/main.c
@@ -1,23 +1,34 @@
+#include "intrinsics.h"
 #include <msp430.h>
 
-#define ENCODER_A BIT4      // P1.4 C1
-#define ENCODER_B BIT5      // P1.5 C2
+#define ENCODER_A BIT4
+#define ENCODER_B BIT5
 
-#define IN_A BIT0           // P4.0 INA1
-#define IN_B BIT1           // P4.1 INA2
+#define IN_A BIT0
+#define IN_B BIT1
 
-#define PWM_OUT BIT6        // P1.6 PWMA
+#define PWM_OUT BIT6
+
+#define I2C_ADDR 0x45
+#define COUNTS_PER_DEGREE_X10 25  // 3.5 counts per degree * 10 for fixed point math
+
+volatile unsigned char message_buffer[2];
+volatile unsigned char msg_index = 0;
 
 volatile int encoder_count = 0;
 unsigned char last_state = 0;
-int speed = 10;
+int speed = 5;
+volatile unsigned int received_degrees = 0;
+int target_position = 0;
 
 void setupUART();
 void uart_send_char(char c);
 void uart_send_int(int num);
+void uart_send_string(const char *str);
 void setupEncoder();
 void setupMotor();
 void setMotor(int dir, int pwm);
+void i2c_slave_setup();
 
 int main(void)
 {
@@ -27,24 +38,15 @@ int main(void)
     setupUART();
     setupEncoder();
     setupMotor();
+    i2c_slave_setup();
 
-    // Startup UART message
-    uart_send_char('U');
-    uart_send_char('A');
-    uart_send_char('R');
-    uart_send_char('T');
-    uart_send_char(' ');
-    uart_send_char('s');
-    uart_send_char('t');
-    uart_send_char('a');
-    uart_send_char('r');
-    uart_send_char('t');
-    uart_send_char('\r');
-    uart_send_char('\n');
+    __enable_interrupt();
+
+    uart_send_string("UART ready\r\n");
 
     while (1)
     {
-        // Read Encoder 
+        // Read encoder
         unsigned char current_state = P1IN & (ENCODER_A | ENCODER_B);
 
         if (current_state != last_state)
@@ -61,51 +63,19 @@ int main(void)
             uart_send_char('\n');
         }
 
-        // Motor Control 
-        if (encoder_count < 1000)
+        // Motor control
+        if (encoder_count < target_position - 2)
         {
-            // Clockwise
             setMotor(1, speed);
         }
-        else if (encoder_count >= 1000)
+        else if (encoder_count > target_position + 2)
         {
-            // Stop
-            setMotor(0, 0);
-            __delay_cycles(10);
-
-            // Now go backward to 0
-            while (encoder_count > 0)
-            {
-                // Read encoder during backward
-                current_state = P1IN & (ENCODER_A | ENCODER_B);
-
-                if (current_state != last_state)
-                {
-                    if ((last_state == ENCODER_A) && (current_state == (ENCODER_A | ENCODER_B)))
-                        encoder_count++; 
-                    else if ((last_state == ENCODER_B) && (current_state == (ENCODER_A | ENCODER_B)))
-                        encoder_count--; 
-
-                    last_state = current_state;
-
-                    uart_send_int(encoder_count);
-                    uart_send_char('\r');
-                    uart_send_char('\n');
-                }
-
-                // Backward
-                if (encoder_count > 0)
-                    setMotor(-1, speed);
-                else
-                    setMotor(0, 0);
-
-                __delay_cycles(10);
-            }
-
-            __delay_cycles(10);  // Pause before starting over
+            setMotor(-1, speed);
         }
-
-        __delay_cycles(10);
+        else
+        {
+            setMotor(0, 0);
+        }
     }
 }
 
@@ -121,6 +91,40 @@ void setupUART(){
     UCA1CTLW0 &= ~UCSWRST;
 }
 
+void i2c_slave_setup() {
+    UCB1CTLW0 |= UCSWRST;
+
+    P4SEL1 &= ~(BIT6 | BIT7);
+    P4SEL0 |= (BIT6 | BIT7);
+
+    UCB1CTLW0 = UCSWRST | UCMODE_3 | UCSYNC;
+    UCB1CTLW0 &= ~UCMST;
+    UCB1I2COA0 = I2C_ADDR | UCOAEN;
+    UCB1CTLW1 = 0;
+
+    UCB1CTLW0 &= ~UCSWRST;
+    UCB1IE |= UCRXIE0 | UCSTPIE;
+}
+
+#pragma vector=EUSCI_B1_VECTOR
+__interrupt void I2C_ISR(void) {
+    switch (__even_in_range(UCB1IV, USCI_I2C_UCBIT9IFG)) {
+        case USCI_I2C_UCRXIFG0:
+            message_buffer[msg_index++] = UCB1RXBUF;
+            break;
+
+        case USCI_I2C_UCSTPIFG:
+            if (msg_index == 2) {
+                received_degrees = (message_buffer[1] & 0xFF) | (message_buffer[0] << 8);
+                target_position = (received_degrees * COUNTS_PER_DEGREE_X10) / 10;
+            }
+            msg_index = 0;
+            break;
+
+        default: break;
+    }
+}
+
 void setupEncoder(){
     P1DIR &= ~(ENCODER_A | ENCODER_B);
     P1REN |= (ENCODER_A | ENCODER_B);
@@ -130,7 +134,6 @@ void setupEncoder(){
 }
 
 void setupMotor(){
-    // PWM setup P1.6
     P1DIR |= PWM_OUT;
     P1SEL1 |= PWM_OUT;
     P1SEL0 &= ~PWM_OUT;
@@ -140,14 +143,12 @@ void setupMotor(){
     TB0CCR1 = 0;
     TB0CTL = TBSSEL_1 | MC_1 | TBCLR;
 
-    // Direction pins P4.0 / P4.1
     P4DIR |= (IN_A | IN_B);
     P4OUT &= ~(IN_A | IN_B);
 }
 
 void setMotor(int dir, int pwm)
 {
-    // Direction
     if (dir > 0)
     {
         P4OUT &= ~IN_A;
@@ -163,7 +164,6 @@ void setMotor(int dir, int pwm)
         P4OUT &= ~(IN_A | IN_B);
     }
 
-    // PWM Duty
     TB0CCR1 = pwm;
 }
 
@@ -181,7 +181,15 @@ void uart_send_int(int num)
         num = -num;
     }
 
+    if (num >= 10000) uart_send_char('0' + (num / 10000) % 10);
+    if (num >= 1000) uart_send_char('0' + (num / 1000) % 10);
     if (num >= 100) uart_send_char('0' + (num / 100) % 10);
     if (num >= 10) uart_send_char('0' + (num / 10) % 10);
     uart_send_char('0' + (num % 10));
+}
+
+void uart_send_string(const char *str)
+{
+    while (*str)
+        uart_send_char(*str++);
 }
